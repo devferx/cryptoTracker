26d01a95be22d2dbb13cc44a6dd39de3812a666e
devferx <ferkki@hotmail.com>
2020-12-07 15:38:31 -0400

    Clase Configuración de la navegación en react native

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
