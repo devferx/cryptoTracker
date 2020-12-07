150f8d328ffd195283f0b0753f5d9491a241c1bf
devferx <ferkki@hotmail.com>
2020-12-07 16:03:34 -0400

    Clase Navegación entre pantallas

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,14 +1,44 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
 class CoinsScreen extends Component {
+  handlePress = () => {
+    console.log('go to detail', this.props);
+
+    this.props.navigation.navigate('CoinDetail');
+  };
+
   render() {
     return (
-      <View>
-        <Text>Coins Screen</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>Coins Screen</Text>
+        <Pressable style={styles.btn} onPress={this.handlePress}>
+          <Text style={styles.btnText}>Ir a detail</Text>
+        </Pressable>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    color: '#FFF',
+    textAlign: 'center',
+  },
+  btn: {
+    padding: 8,
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    margin: 16,
+  },
+  btnText: {
+    color: '#FFF',
+    textAlign: 'center',
+  },
+});
 
 export default CoinsScreen;
